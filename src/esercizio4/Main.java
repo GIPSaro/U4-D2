cd057b8150f6cb61b0c71f6afe40643dfae9929b
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-07-23 16:45:49 +0200

    commit esercizio 4

--- src/esercizio4/Main.java
+++ src/esercizio4/Main.java
@@ -1,4 +1,20 @@
 package esercizio4;
 
-public class Main {
-}
+import java.util.Scanner;
+
+    public class Main {
+
+        public static void main(String[] args) {
+
+            Scanner in = new Scanner(System.in);
+            System.out.println("Inserisci numero:");
+            int numero = in.nextInt();
+
+            for (int i = numero; i >= 0 ; i--) {
+                System.out.println(i);
+            }
+
+            in.close();
+        }
+
+    }

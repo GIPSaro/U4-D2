ab4de09f0bbf197b7cef19b6a41b4967f421ab6f
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-07-23 16:10:39 +0200

    commit esercizio 2

--- src/esercizio2/Main.java
+++ src/esercizio2/Main.java
@@ -5,6 +5,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        
+
+
+        System.out.println("Inserisci un numero compreso tra 0 e 3: ");
+        int num = scanner.nextInt();
+        String numeroInChars;
+
+        switch (num){
+            case 0:
+            numeroInChars = "zero";
+            break;
+            case 1:
+                numeroInChars ="uno";
+                break;
+            case 2:
+                numeroInChars ="due";
+                break;
+            case 3:
+                numeroInChars ="tre";
+                break;
+            default:
+                System.out.println("Il numero non è compreso tra 0 e 3!!");
+                scanner.close();
+                return;
+
+        }
+        System.out.println(numeroInChars);
+        scanner.close();
     }
 }
